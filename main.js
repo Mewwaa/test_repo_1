e589cfc41f3d49eadeecdcb81a175f99b9fd0b2c
PCinkusz <pcinkusz123321@gmail.com>
2021-06-01 12:25:32 +0200

    LISTA

--- main.js
+++ main.js
@@ -4,9 +4,16 @@ import ReactDOM from 'react-dom';  // import potrzebnych bibliotek
 <<<<<<< HEAD
 import Comp1 from './comp1.jsx';
 import Buto from "./button.jsx";
+<<<<<<< HEAD
 =======
 // import List from '.test_repo_1/list.jsx';
 >>>>>>> 9c0eb74 (LISTA)
+=======
+// import List from '.test_repo_1/list.jsx';
+
+
+
+>>>>>>> e3cdbbf (LISTA)
 
 
 ReactDOM.render(
