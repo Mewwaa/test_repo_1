5bd461ca7f7f4f0ea9472d6a37073625d0e1331a
Bartek <bartlomiej.gedek@zgierz.edu.pl>
2021-06-01 12:34:19 +0200

    Paragraph

--- react.jsx
+++ react.jsx
@@ -3,12 +3,16 @@ import React from 'react'; // import potrzebnych bibliotek
 class website extends React.Component { // klasa będąca komponentem
   constructor(props) { // konstruktor klasy, przechowuje stan danego komponentu
     super(props);
+    this.state = {
+      klasa: "2PUp"
+    }
   }
 
   render() { // to będzie zwracane w htmlu!
     return (
       <div>
         Hello Again!
+        <p>Hello class</p>{this.state.klasa}
       </div>
     );
   }
