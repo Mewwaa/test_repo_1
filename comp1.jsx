32b032cd313080a66d2bfafc49a2fdfd311a814c
Bartek <bartlomiej.gedek@zgierz.edu.pl>
2021-06-01 14:19:44 +0200

    third commit

--- comp1.jsx
+++ comp1.jsx
@@ -5,16 +5,16 @@ class Comp1 extends React.Component { // klasa będąca komponentem
     super(props);
   }
 
-  yes() {
-    console.log("Hello");
+  vixa() {
+    console.log("Dobry wieczor Polska");
   }
 
   render() { // to będzie zwracane w htmlu!
     return (
       <div>
         <b>Hi there!</b>
-        <p>Hello</p>
-        <button onClick={() => this.yes()}>Click me!</button>
+        <b>Hello</b>
+        <button onClick={() => this.vixa()}>Click me!</button>
       </div>
     );
   }
