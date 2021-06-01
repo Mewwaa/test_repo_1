3bc4e067d98d0e588f0bbaf577faefa4a94d44fd
PCinkusz <pcinkusz123321@gmail.com>
2021-06-01 14:28:08 +0200

    dasdA

---
+++ list.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+ 
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return (
+            <div>
+                <ul>
+                    <li>a</li>
+                    <li>b</li>
+                    <li>c</li>
+                </ul>
+            </div>
+        )   
+    }
+}
+ 
+
+ 
+export default List;
--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import React from 'react'; // import potrzebnych bibliotek
 import ReactDOM from 'react-dom';  // import potrzebnych bibliotek
 
+<<<<<<< HEAD
 import Comp1 from './comp1.jsx';
 import Buto from "./button.jsx";
+=======
+// import List from '.test_repo_1/list.jsx';
+>>>>>>> 9c0eb74 (LISTA)
 
 
 ReactDOM.render(
